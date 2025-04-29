96f975491ec66080f77ed1145818f2e8e79eaecf
Will Feng <yfeng.us@gmail.com>
2025-04-28 20:28:57 -0700

    make conda solver faster

--- .github/scripts/install_triton.sh
+++ .github/scripts/install_triton.sh
@@ -8,7 +8,9 @@ set -ex
     git clone https://github.com/triton-lang/triton.git  # install triton latest main
     (
         pushd triton/
-        conda install -y -c conda-forge gcc_linux-64=12 gxx_linux-64=12 gcc=12 gxx=12
+        conda install -y -n venv conda=25.3.1 conda-libmamba-solver -c conda-forge
+        conda config --set solver libmamba
+        conda install -y -c conda-forge gcc_linux-64=13 gxx_linux-64=13 gcc=13 gxx=13
         pip install -r python/requirements.txt
         pip install .  # install to conda site-packages/ folder
         popd
