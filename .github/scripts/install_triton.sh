7e5caaf9c350c66e039132fbf164f67572ccf536
Will Feng <yfeng.us@gmail.com>
2025-04-29 12:10:05 -0700

    try MAX_JOBS=1 TRITON_PARALLEL_LINK_JOBS=1 when building triton

--- .github/scripts/install_triton.sh
+++ .github/scripts/install_triton.sh
@@ -27,7 +27,7 @@ then
             conda config --set solver libmamba
             conda install -y -c conda-forge gcc_linux-64=13 gxx_linux-64=13 gcc=13 gxx=13
             pip install -r python/requirements.txt
-            pip install .  # install to conda site-packages/ folder
+            MAX_JOBS=1 TRITON_PARALLEL_LINK_JOBS=1 pip install .  # install to conda site-packages/ folder
             popd
         )
         rm -rf triton/
@@ -36,21 +36,21 @@ then
     exit 0
 fi
 
-if [ "$ACTION" = "python-stub-only" ];
-then
-    set -ex
-    (
-        mkdir -p /tmp/$USER
-        pushd /tmp/$USER
-        pip uninstall -y triton pytorch-triton || true
-        rm -rf triton/ || true
-        git clone https://github.com/triton-lang/triton.git  # install triton latest main
-        # NOTE: Unfortunately building triton from source will crash the CPU CI machines (c5.2xlarge or c5.4xlarge).
-        # But since for lint jobs we actually don't need the .so files, we can just install and use the python stubs.
-        export SITE_PKGS_PATH=`python -c "import sysconfig, json; print(sysconfig.get_paths()['purelib'])"`
-        mkdir ${SITE_PKGS_PATH}/triton
-        cp -r triton/python/triton/* ${SITE_PKGS_PATH}/triton
-        rm -rf triton/
-    )
-    exit 0
-fi
+# if [ "$ACTION" = "python-stub-only" ];
+# then
+#     set -ex
+#     (
+#         mkdir -p /tmp/$USER
+#         pushd /tmp/$USER
+#         pip uninstall -y triton pytorch-triton || true
+#         rm -rf triton/ || true
+#         git clone https://github.com/triton-lang/triton.git  # install triton latest main
+#         # NOTE: Unfortunately building triton from source will crash the CPU CI machines (c5.2xlarge or c5.4xlarge).
+#         # But since for lint jobs we actually don't need the .so files, we can just install and use the python stubs.
+#         export SITE_PKGS_PATH=`python -c "import sysconfig, json; print(sysconfig.get_paths()['purelib'])"`
+#         mkdir ${SITE_PKGS_PATH}/triton
+#         cp -r triton/python/triton/* ${SITE_PKGS_PATH}/triton
+#         rm -rf triton/
+#     )
+#     exit 0
+# fi
