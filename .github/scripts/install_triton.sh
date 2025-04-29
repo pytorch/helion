8046541fe4ebac4a80afb08a504e44cb90d53d38
Will Feng <yfeng.us@gmail.com>
2025-04-29 13:25:43 -0700

    comment

--- .github/scripts/install_triton.sh
+++ .github/scripts/install_triton.sh
@@ -27,6 +27,7 @@ then
             conda config --set solver libmamba
             conda install -y -c conda-forge gcc_linux-64=13 gxx_linux-64=13 gcc=13 gxx=13
             pip install -r python/requirements.txt
+            # Use TRITON_PARALLEL_LINK_JOBS=2 to avoid OOM on CPU CI machines
             MAX_JOBS=$(nproc) TRITON_PARALLEL_LINK_JOBS=2 pip install .  # install to conda site-packages/ folder
             popd
         )
