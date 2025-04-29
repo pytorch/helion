9e7db944010a399e08ea2bade464aa1a4a180c0c
Will Feng <yfeng.us@gmail.com>
2025-04-28 21:47:44 -0700

    try turn off mamba solver

--- .github/scripts/install_triton.sh
+++ .github/scripts/install_triton.sh
@@ -8,8 +8,8 @@ set -ex
     git clone https://github.com/triton-lang/triton.git  # install triton latest main
     (
         pushd triton/
-        conda install -y -n venv conda=25.3.1 conda-libmamba-solver -c conda-forge
-        conda config --set solver libmamba
+        # conda install -y -n venv conda=25.3.1 conda-libmamba-solver -c conda-forge
+        # conda config --set solver libmamba
         conda install -y -c conda-forge gcc_linux-64=13 gxx_linux-64=13 gcc=13 gxx=13
         pip install -r python/requirements.txt
         pip install .  # install to conda site-packages/ folder
