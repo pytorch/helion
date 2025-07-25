36455d0a22389a3b9b358a35bec1c29299c720be
Will Feng <yfeng.us@gmail.com>
2025-07-25 01:16:16 -0700

    improve run_input_shard.sh script

--- benchmarks/run_input_shard.sh
+++ benchmarks/run_input_shard.sh
@@ -1,1 +1,4 @@
-CUDA_VISIBLE_DEVICES=${RANK} python benchmarks/run.py --input-shard $((RANK+1))/${WORLD_SIZE} >benchmarks_autotune_$(date +%s)_input_shard_$((RANK+1))_of_${WORLD_SIZE}.txt 2>&1
+[[ -z "$RANK_OFFSET" ]] && { echo "Error: RANK_OFFSET is not set"; exit 1; }
+[[ -z "$SHARD" ]] && { echo "Error: SHARD is not set"; exit 1; }
+[[ -z "$WORLD_SIZE" ]] && { echo "Error: WORLD_SIZE is not set"; exit 1; }
+CUDA_VISIBLE_DEVICES=$((RANK_OFFSET+SHARD)) python benchmarks/run.py --input-shard $((SHARD+1))/${WORLD_SIZE} --metrics tflops,gbps,speedup >benchmarks_autotune_$(date +%s)_input_shard_$((SHARD+1))_of_${WORLD_SIZE}.txt 2>&1
