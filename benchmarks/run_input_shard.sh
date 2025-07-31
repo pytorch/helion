07519c3d9ab93d3753794fcaba2ded53c91f7625
Will Feng <yfeng.us@gmail.com>
2025-07-31 12:36:56 -0700

    output to CSV folder; change SHARD to start from 1

--- benchmarks/run_input_shard.sh
+++ benchmarks/run_input_shard.sh
@@ -4,23 +4,25 @@
 
 # Capture timestamp once for consistent filename
 TIMESTAMP=$(date +%s)
-OUTPUT_FILE="benchmarks_autotune_${TIMESTAMP}_input_shard_$((SHARD+1))_of_${WORLD_SIZE}.txt"
+OUTPUT_FILE="benchmarks_autotune_${TIMESTAMP}_input_shard_${SHARD}_of_${WORLD_SIZE}.txt"
+CSV_OUTPUT_DIR="benchmarks_autotune_${TIMESTAMP}_input_shard_${SHARD}_of_${WORLD_SIZE}_csv"
 
 # Retry until success
 attempt=0
 while true; do
 # while (( attempt < 10 )); do
     attempt=$((attempt + 1))
-    echo "Attempt $attempt: Running benchmark for shard $((SHARD+1))/${WORLD_SIZE}..."
+    echo "Attempt $attempt: Running benchmark for shard ${SHARD}/${WORLD_SIZE}..."
 
     # TIMESTAMP=$(date +%s)
-    # OUTPUT_FILE="benchmarks_autotune_${TIMESTAMP}_input_shard_$((SHARD+1))_of_${WORLD_SIZE}.txt"
+    # OUTPUT_FILE="benchmarks_autotune_${TIMESTAMP}_input_shard_${SHARD}_of_${WORLD_SIZE}.txt"
 
-    CUDA_VISIBLE_DEVICES=$((RANK_OFFSET+SHARD)) python benchmarks/run.py --input-shard $((SHARD+1))/${WORLD_SIZE} --metrics accuracy,tflops,gbps,speedup >"$OUTPUT_FILE" 2>&1
+    mkdir -p ${CSV_OUTPUT_DIR} || true
+    CUDA_VISIBLE_DEVICES=$((RANK_OFFSET+SHARD-1)) python benchmarks/run.py --input-shard ${SHARD}/${WORLD_SIZE} --metrics accuracy,tflops,gbps,speedup --csv --output-dir ${CSV_OUTPUT_DIR} >"$OUTPUT_FILE" 2>&1
 
     exit_code=$?
     if [ $exit_code -eq 0 ]; then
-        echo "Success! Benchmark completed for shard $((SHARD+1))/${WORLD_SIZE}"
+        echo "Success! Benchmark completed for shard ${SHARD}/${WORLD_SIZE}"
         break
     else
         echo "Failed with exit code $exit_code. Retrying..."
@@ -28,4 +30,5 @@ while true; do
     fi
 done
 
-# SHARD=0 RANK_OFFSET=4 WORLD_SIZE=4 bash benchmarks/run_input_shard.sh
+# Runs the 1st shard of input on GPU-0:
+# SHARD=1 RANK_OFFSET=4 WORLD_SIZE=4 bash benchmarks/run_input_shard.sh
