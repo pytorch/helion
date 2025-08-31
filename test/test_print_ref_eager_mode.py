c2d79a3746fc0ded42415781f484f4b89aecf2b0
Sibylau <jieeliu@meta.com>
2025-08-30 22:58:43 -0700

    fix CI issues, use ref_eager_mode for better naming

--- test/test_print_ref_eager_mode.py
+++ test/test_print_ref_eager_mode.py
@@ -4,20 +4,20 @@ import contextlib
 import io
 import unittest
 
+import helion
+import helion.language as hl
+
 import pytest
 import torch
-
-import helion
 from helion import exc
 from helion._testing import TestCase
-import helion.language as hl
 
 
 class TestPrintOutputCode(TestCase):
     def test_ref_eager_mode_code_print_error(self):
         """Test that RefEagerModeCodePrintError is raised when using @helion.kernel with both settings"""
 
-        with pytest.raises(exc.EagerCodePrintError):
+        with pytest.raises(exc.RefEagerModeCodePrintError):
 
             @helion.kernel(
                 use_default_config=True,
@@ -39,7 +39,7 @@ class TestPrintOutputCode(TestCase):
             y = torch.randn([512, 512], device="cuda", dtype=torch.float16)
             torch.testing.assert_close(add(x, y), torch.add(x, y))
 
-    def test_normal_code_print(self):
+    def test_normal_mode_code_print(self):
         """Test that output code is in stderr when using @helion.kernel with normal mode"""
 
         f = io.StringIO()
@@ -48,6 +48,7 @@ class TestPrintOutputCode(TestCase):
             @helion.kernel(
                 use_default_config=True,
                 print_output_code=True,
+                ref_mode=helion.RefMode.OFF,
             )
             def add(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
                 x, y = torch.broadcast_tensors(x, y)
