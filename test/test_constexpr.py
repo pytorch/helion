dc0c75557ff05813676d3c70592c85cce2d0696d
Wang, Eikan <eikan.wang@intel.com>
2025-10-20 12:35:24 +0000

    Fix lint issue

--- test/test_constexpr.py
+++ test/test_constexpr.py
@@ -10,7 +10,6 @@ from helion._testing import RefEagerTestBase
 from helion._testing import TestCase
 from helion._testing import code_and_output
 from helion._testing import skipIfRefEager
-from helion._testing import skipIfXPU
 import helion.language as hl
 
 
