2a6570186ca7664f239905a36e0c68ca85a8c238
Ethan Che <eche@fb.com>
2025-11-16 10:32:24 -0800

    revert linter changes

--- helion/autotuner/__init__.py
+++ helion/autotuner/__init__.py
@@ -1,31 +1,25 @@
 from __future__ import annotations
 
-from .config_fragment import (
-    BooleanFragment as BooleanFragment,
-    EnumFragment as EnumFragment,
-    IntegerFragment as IntegerFragment,
-    ListOf as ListOf,
-    PowerOfTwoFragment as PowerOfTwoFragment,
-)
+from .bayes_opt import UCBPatternSearch
+from .config_fragment import BooleanFragment as BooleanFragment
+from .config_fragment import EnumFragment as EnumFragment
+from .config_fragment import IntegerFragment as IntegerFragment
+from .config_fragment import ListOf as ListOf
+from .config_fragment import PowerOfTwoFragment as PowerOfTwoFragment
 from .config_spec import ConfigSpec as ConfigSpec
 from .de_surrogate_hybrid import DESurrogateHybrid as DESurrogateHybrid
 from .differential_evolution import (
     DifferentialEvolutionSearch as DifferentialEvolutionSearch,
 )
-from .effort_profile import (
-    AutotuneEffortProfile as AutotuneEffortProfile,
-    DifferentialEvolutionConfig as DifferentialEvolutionConfig,
-    PatternSearchConfig as PatternSearchConfig,
-    RandomSearchConfig as RandomSearchConfig,
-)
+from .effort_profile import AutotuneEffortProfile as AutotuneEffortProfile
+from .effort_profile import DifferentialEvolutionConfig as DifferentialEvolutionConfig
+from .effort_profile import PatternSearchConfig as PatternSearchConfig
+from .effort_profile import RandomSearchConfig as RandomSearchConfig
 from .finite_search import FiniteSearch as FiniteSearch
-from .local_cache import (
-    LocalAutotuneCache as LocalAutotuneCache,
-    StrictLocalAutotuneCache as StrictLocalAutotuneCache,
-)
+from .local_cache import LocalAutotuneCache as LocalAutotuneCache
+from .local_cache import StrictLocalAutotuneCache as StrictLocalAutotuneCache
 from .pattern_search import PatternSearch as PatternSearch
 from .random_search import RandomSearch as RandomSearch
-from .ucb_pattern_search import UCBPatternSearch
 
 search_algorithms = {
     "DESurrogateHybrid": DESurrogateHybrid,
