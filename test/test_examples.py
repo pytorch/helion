24c94ce5b1aac0ed84164c14520759eb3cb0c808
Umesh Chand <umechand@amd.com>
2026-02-16 14:29:56 -0800

    Fix test isolation: restore fp32_precision after test_matmul and test_examples (#1412)

--- test/test_examples.py
+++ test/test_examples.py
@@ -24,8 +24,21 @@ from helion._testing import skipIfRocm
 from helion._testing import skipIfTileIR
 from helion._testing import skipIfXPU
 
-torch.backends.cuda.matmul.fp32_precision = "tf32"
-torch.backends.cudnn.conv.fp32_precision = "tf32"
+_orig_matmul_fp32_precision: str = "none"
+_orig_cudnn_fp32_precision: str = "none"
+
+
+def setUpModule() -> None:
+    global _orig_matmul_fp32_precision, _orig_cudnn_fp32_precision
+    _orig_matmul_fp32_precision = torch.backends.cuda.matmul.fp32_precision
+    _orig_cudnn_fp32_precision = torch.backends.cudnn.conv.fp32_precision
+    torch.backends.cuda.matmul.fp32_precision = "tf32"
+    torch.backends.cudnn.conv.fp32_precision = "tf32"
+
+
+def tearDownModule() -> None:
+    torch.backends.cuda.matmul.fp32_precision = _orig_matmul_fp32_precision
+    torch.backends.cudnn.conv.fp32_precision = _orig_cudnn_fp32_precision
 
 
 @onlyBackends(["triton"])
