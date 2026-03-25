e41eb30a4c21bccfa2585b6ab027eff299df91fd
Umesh Chand <umechand@amd.com>
2026-03-25 21:49:10 +0000

    lint fix

--- test/test_register_tunable.py
+++ test/test_register_tunable.py
@@ -13,7 +13,6 @@ from helion._testing import RefEagerTestBase
 from helion._testing import TestCase
 from helion._testing import code_and_output
 from helion._testing import onlyBackends
-from helion._testing import skipIfRocm
 from helion._testing import skipIfSharedMemoryLessThan
 from helion._testing import xfailIfCute
 from helion.autotuner import EnumFragment
