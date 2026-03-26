8d5f5c764bffc332b66b29cb49dfb2ad655c4819
Umesh Chand <umechand@amd.com>
2026-03-26 22:43:10 +0000

    Putting skip back till Rocm triton bug is fixed.

--- test/test_examples.py
+++ test/test_examples.py
@@ -1617,7 +1617,7 @@ class TestExamples(RefEagerTestBase, TestCase):
     @xfailIfCute(
         "CuTe squeeze-and-excitation forward still exceeds thread-block limits"
     )
-    @skipIfRocm("failure on rocm")
+    @skipIfRocm("Triton ROCm store-forwarding bug: stale global memory reads")
     @skipIfCudaSharedMemoryLessThan(
         131072, reason="block sizes exceed device shared memory limit"
     )
